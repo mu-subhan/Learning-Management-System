65b38dc256c2f4fecb79a6c084764f745707a2fb
Muhammad Subhan <muhammadsubhan8934@gmail.com>
2025-09-09 18:47:25 +0500

    correct registration path

--- server/routes/user.routes.ts
+++ server/routes/user.routes.ts
@@ -4,7 +4,7 @@ import {  authorizeRoles, isAuthenticated } from "../middleware/auth";
 
 const userRouter = express.Router();
 
-userRouter.post("/register", registrationUser);
+userRouter.post("/registration", registrationUser);
 userRouter.post("/activate-user",activateUser );
 userRouter.post("/login",loginUser );
 userRouter.get("/logout",isAuthenticated,logoutUser );
