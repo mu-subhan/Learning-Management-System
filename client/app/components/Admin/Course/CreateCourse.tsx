665a886fc2532d86d1e1fbb7529648ab83a6b277
Muhammad Subhan <muhammadsubhan8934@gmail.com>
2025-09-18 18:27:46 +0500

    create course feature

--- client/app/components/Admin/Course/CreateCourse.tsx
+++ client/app/components/Admin/Course/CreateCourse.tsx
@@ -1,16 +1,36 @@
 'use client'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import CourseInformation from "./CourseInformation";
 import CourseOptions from "./CourseOptions";
 import CourseData from "./CourseData";
 import CourseContent from "./CourseContent";
 import CoursePreview from "./CoursePreview";
+import { useCreateCourseMutation } from '@/redux/features/course/coursesApi';
+import toast from 'react-hot-toast';
+import { redirect } from 'next/navigation';
 
 
 type Props = {}
 
 const CreateCourse = (props: Props) => {
-      const [active, setActive] = useState(0);
+  const [createCourse,{isLoading,isSuccess,error}] = useCreateCourseMutation();
+
+
+  useEffect(()=>{
+    if(isSuccess){
+      toast.success("Course created Successfully")
+     redirect("/admin/all-course");
+    }
+    if(error){
+      if("data" in error){
+      const errorMessage = error as any;
+
+      toast.error(errorMessage.data.message)
+    }
+    }
+  },[isLoading,isSuccess,error])
+
+  const [active, setActive] = useState(0);
   const [courseInfo, setCourseInfo] = useState({
     name: "",
     description: "",
@@ -43,7 +63,7 @@ const CreateCourse = (props: Props) => {
   const [courseData, setCourseData] = useState({});
 
 
- const handleSubmit = async () => {
+  const handleSubmit = async () => {
     //format benfits array
     const formattedBenefits = benefits.map((benefit) => ({
       title: benefit.title,
@@ -86,8 +106,11 @@ const CreateCourse = (props: Props) => {
 
   // console.log(courseData)
 
-  const handleCourseCreate = async(e:any)=>{
-const data = courseData
+  const handleCourseCreate = async (e: any) => {
+    const data = courseData;
+    if(!isLoading){
+      await createCourse(data);
+    }
   }
   return (
     <div className="w-full flex min-h-screen">
@@ -112,8 +135,8 @@ const data = courseData
         )}
         {active === 2 && (
           <CourseContent
-          active={active}
-          setActive={setActive}
+            active={active}
+            setActive={setActive}
             courseContentData={courseContentData}
             setCourseContentData={setCourseContentData}
             handleSubmit={handleSubmit}
