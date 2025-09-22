58c597b42f390490f5ab3aa8886e8b1814803666
Muhammad Subhan <muhammadsubhan8934@gmail.com>
2025-09-22 12:42:28 +0500

    remove catched users

--- server/controllers/course.controller.ts
+++ server/controllers/course.controller.ts
@@ -114,23 +114,16 @@ export const getSingleCourse = CatchAsyncError(async(req:Request,res:Response,ne
 
 export const getAllCourses = CatchAsyncError(async(req:Request,res:Response,next:NextFunction)=>{
     try {
-        const isCacheExist = await redis.get("allCourses"); 
-       if (isCacheExist) {
-        const courses = JSON.parse(isCacheExist);
-        return res.status(200).json({
-            success:true,
-            courses
-        });
-       } else {
+       
          const courses = await CourseModel.find().select("-courseData.videoUrl -courseData.suggestion -courseData.questions -courseData.links")
-            await redis.set("allCourses", JSON.stringify(courses));
+            // await redis.set("allCourses", JSON.stringify(courses));
         res.status(200).json({
             success:true,
             courses
         })
        }
 
-    } catch (error:any) {
+    catch (error:any) {
         return next(new ErrorHandler(error.message,500));
     }
 
